78c552c0c5aaeb93f40b75001d2e1e91c2dcb395
Taeshi <a86487817@gmail.com>
2013-08-18 18:57:18 +0800

    modify inbox look change footer to fix buttom

--- js/bulletin.js
+++ js/bulletin.js
@@ -1,2 +1,14 @@
 // remove unuse stuff
 $('#logo').remove();
+
+// change menu
+$('#menu')[0].setAttribute('style', 'margin:10px;');
+for(var k in $('#menu > a')) {
+	$('#menu > a')[k].setAttribute('class', 'btn btn-info');
+}
+
+// change fieldset
+if( $('fieldset').size() != 0 ){
+	$('fieldset')[0].setAttribute('class', 'well');
+	$('fieldset')[0].setAttribute('style', '');
+}
